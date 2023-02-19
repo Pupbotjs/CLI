5f2c391dbd09e35e1ae7efa1323ebaf92219fbc4
Dogxi <106546046+dog234@users.noreply.github.com>
2023-02-19 12:03:51 +0800

    更新 模版

--- templates/javascript.js
+++ templates/javascript.js
@@ -2,15 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.js_template = void 0;
 exports.js_template = `
-const { PupPlugin, segment, http } = require('@pupbot/core')
+const { PupPlugin, segment } = require('@pupbot/core')
 
 const plugin = new PupPlugin('demo', '0.1.0')
-//插件被启用调用函数
+//插件挂载
 plugin.onMounted(() => {
   plugin.onMessage(event => {
     const { raw_message } = event
-//event为接收到的(消息/事件)对象
-    if (raw_message === 'hello') {   //如果接受到的消息为hello，回复world
+    if (raw_message === 'hello') { 
       const msgs = [segment.face(66), 'world']
       event.reply(msgs)
     }
