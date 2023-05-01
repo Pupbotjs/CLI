50398edf2f37b93b469fd0853d660510b83c74fc
Dogxi <106546046+dog234@users.noreply.github.com>
2023-05-01 09:48:51 +0800

    更新 逻辑

--- utils/getCurrentAccount.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCurrentAccount = void 0;
-const path_1 = require("../path");
-function getCurrentAccount() {
-    const pupConf = require(path_1.ConfPath);
-    return String(pupConf?.account ?? '');
-}
-exports.getCurrentAccount = getCurrentAccount;
